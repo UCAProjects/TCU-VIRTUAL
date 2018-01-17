8dc32fea6c4cb649c8d1357b7c90317fe3818ff7
albinmora <albin.moravalverde@gmail.com>
2018-01-17 16:14:05 -0600

    Plantilla Heade Footer

---
+++ tcu/vistas/plantilla_Paginas.php
@@ -0,0 +1,22 @@
+﻿<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <title>
+        	Registro  <!--  MODIFICAR TITLE -->
+        </title>
+    </head>
+    <body>
+
+    	<?php 
+    		include '../header.php'
+    	?>
+        
+
+      <!--  CODGO DE LA PAGINA -->
+
+
+        <?php 
+        	include '../footer.php'
+        ?>
+    </body>
+</html>
