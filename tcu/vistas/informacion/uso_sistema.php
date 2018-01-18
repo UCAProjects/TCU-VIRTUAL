0e29c862cd9eee71a20a35d235bd69da8b3d756a
albinmora <albin.moravalverde@gmail.com>
2018-01-18 17:59:22 -0600

    Revert "Merge branch 'master' of https://github.com/albinmora/TCU-VIRTUAL"
    This reverts commit 21c4e68173729784604b8807b0cd5dd3ae268959, reversing
    changes made to 8931c0e14391fbfea5ce4be90d3b0ce4a2b97b63.

--- tcu/vistas/informacion/uso_sistema.php
+++ tcu/vistas/informacion/uso_sistema.php
@@ -1,7 +1,33 @@
 <!doctype html>
 <html class="no-js" lang="">
-  <!--.HEADER-->
-  <?php include("../../header.php") ?>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Tcu Virtual</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="../../css/normalize.css">
+        <link rel="stylesheet" href="../../css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald|PT+Sans" rel="stylesheet">
+        <link rel="stylesheet" href="../../css/main.css">
+        <link rel="stylesheet" href="../../css/informacion.css">
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <header class="site-header">
+          <div class="fondo-encabezado">
+
+          </div><!-- .fondo-encabezado -->
+        </header>
 
         <main class="site-main">
           <section class="seccion-informacion">
@@ -10,31 +36,54 @@
               <div class="instrucciones-generales">
                 <h3>Acerca del Sistema</h3>
                 <p>
-                  El sistema de Tcu Virtual es una iniciativa de la universidad para facilitar,
-                  el desarrollo del Trabajo Comunal Universitario del estudiantado, esta propusta
-                  pretende poder digitalizar la informacion y que la misma lleve un orden mas coherente
-                  en todo el proceso de desarrollo y sea una herramienta que apoye al estudiante en este
-                  importante proceso como parte de su formación integral y como parte importante para poder
-                  concluir con su plan de estudios.
+                    Texto
                 </p>
-                <h3>Registro e ingreso al sistema</h3>
+                <h3>Registro e ingreso al sitema</h3>
                 <p>
-                  Antes de ingresar al sistema debe registrarse en el mismo, debe presionar el botón de registro,
-                  que lo redirijirá a la sección donde podrá ingresar sus datos personales, así como un usuario y contraseña para poder ingresar
-                  al sistema. Es importante que brinde una dirección de correo válido, para todo lo referente al
-                  proceso de desarrollo del Trabajo Comunal Universitario.
+                    Texto
                 </p>
                 <h3>Datos del proyecto</h3>
                 <p>
-                  Una vez que se halla registrado en el sistema, debe digitar su usuario y contraseña para ingresar al sistema, que lo llevará
-                  a la pantalla de datos del proyecto.
+                    Texto
                 </p>
               </div>
             </div><!--.contenedor-->
-          </section><!--.seccion-informacion-->
+          </section><!--.precios seccion-->
         </main>
 
-        <!--.FOOTER-->
-        <?php include("../../footer.php") ?>
+        <footer class="site-footer">
+          <div class="contenedor clearfix">
+              <div class="footer-informacion">
+                <h3>Sobre <span>Tcu Virtual</span></h3>
+                <p>Plataforma virtual para la gestión del desarrollo del trabajo comunal universitario de las y los estudiantes, de la Universidad
+                   Florencio del Castillo.
+                </p>
+              </div><!--.footer-informacion-->
+              <div class="menu">
+                <nav class="redes-sociales">
+                  <h3>Redes <span>sociales</span></h3>
+                  <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                  <a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+                  <a href="#"><i class="fa fa-globe" aria-hidden="true"></i></a>
+                </nav>
+              </div><!--.menu-->
+          </div><!--.contenedor clearfix-->
+          <p class="copyright">
+            Todos los derechos reservados TCU VIRTUAL 2018
+          </p>
+        </footer>
+
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
     </body>
 </html>
