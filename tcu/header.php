d49c0c66cd99cc4c1fa482586557a26bd84a5394
albinmora <albin.moravalverde@gmail.com>
2018-01-19 15:35:34 -0600

    recuperación

--- tcu/header.php
+++ tcu/header.php
@@ -7,9 +7,6 @@
 
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
         <link rel="manifest" href="../../site.webmanifest">
         <link rel="apple-touch-icon" href="../../icon.png">
         <!-- Place favicon.ico in the root directory -->
