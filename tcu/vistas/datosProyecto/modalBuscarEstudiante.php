ed186a83c3992b501044ac487a13f4668b1e91c2
albinmora <albin.moravalverde@gmail.com>
2018-01-18 18:25:51 -0600

    header footer eliminar

---
+++ tcu/vistas/datosProyecto/modalBuscarEstudiante.php
@@ -0,0 +1,58 @@
+<div class="modal-header" align="center">
+  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+    <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+  </button>
+  <h2><i class="fa fa-search-plus" aria-hidden="true"></i>Buscar Estudiantes</h2>
+  
+</div>
+
+<br>
+<div class="modal.body">
+  <div class="formulario">
+    <div class="row">
+      <div class="col-md-7 col-md-offset-1">
+        <input type="text" name="cedula" id="cedula" placeholder="Buscar Nombre" autocomplete="on" required>
+      </div>
+      <div class="col-md-3">
+        <button  class="" >Buscar</button>
+      </div>
+    </div>
+  </div>
+  <hr>
+                    <table class="table table-striped">
+                    <thead>
+                      <tr>
+                        <th scope="col">Cod</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Apellidos</th>
+                        <th scope="col">Acción</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>Marck</td>
+                        <td>Castro</td>
+                        <td><a href="#"><i class="fa fa-check-circle" aria-hidden="true"></i></a></td>
+                      </tr>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>Marck</td>
+                        <td>Castro</td>
+                        <td><a href="#"><i class="fa fa-check-circle" aria-hidden="true"></i></a></td>
+                      </tr>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>Marck</td>
+                        <td>Castro</td>
+                        <td><a href="#"><i class="fa fa-check-circle" aria-hidden="true"></i></a></td>
+                      </tr>
+                    </tbody>
+                  </table>
+</div>
+  <br>
+
+  <br>
+  <div class="modal-footer">
+  
+</div>
